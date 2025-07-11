b90d6b6ce414cc048aa26ef1d0ba4d668d739154
Creative_Engine_OS <oogimages@gmail.com>
2025-07-11 08:39:46 -0400

    Delete utils directory

--- utils/caption_utils.py
+++
@@ -1,8 +0,0 @@
-from nltk.tokenize import sent_tokenize
-
-def truncate_caption(text):
-    text = str(text).replace("(Photo by Bastiaan Slabbers/NurPhoto)", "").strip()
-    sentences = sent_tokenize(text)
-    short = " ".join(sentences[:2])
-    remainder = " ".join(sentences[2:])
-    return short, remainder
